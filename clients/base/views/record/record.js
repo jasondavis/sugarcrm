f3b9d3acdbd0843dd68cfd7e0e719e683b08f9d5
dimaqw <dimaqw@gmail.com>
2015-07-31 09:23:17 +0300

    Update record.js

--- clients/base/views/record/record.js
+++ clients/base/views/record/record.js
@@ -1,1 +1,9 @@
-alert(1);
+({
+    extendsFrom: 'RecordView',
+
+    initialize: function (options) {
+        app.view.invokeParent(this, {type: 'view', name: 'record', method: 'initialize', args:[options]});
+        //добавляем объект, что бы можно было к нему обращаться из консоли
+        SL_RequestBean = this;
+    }
+})
