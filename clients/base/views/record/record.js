7c40ca800017737839579a4189ac1e32154e3375
dimaqw <dimaqw@gmail.com>
2015-08-06 09:29:20 +0300

    Update record.js

--- clients/base/views/record/record.js
+++ clients/base/views/record/record.js
@@ -5,5 +5,19 @@
         app.view.invokeParent(this, {type: 'view', name: 'record', method: 'initialize', args:[options]});
         //добавляем объект, что бы можно было к нему обращаться из консоли
         SL_RequestBean = this;
+        
+        
+        if(!window.SL_Beans) SL_Beans = {};
+		var SL_Bean = jQuery.extend(true, {}, this);
+		SL_Beans[this.options.module] = SL_Bean;
     }
 })
+
+App.drawer.open({
+                layout:'create',
+                context:{
+                    module:'SL_Requests',model:SL_Beans.SL_Requests.model
+                }
+            }
+            
+        );
