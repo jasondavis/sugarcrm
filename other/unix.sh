d11550b05b9c933300c70b42eee0b84b812a6c09
dimaqw <dimaqw@gmail.com>
2015-08-26 19:18:21 +0300

    Update unix.sh

--- other/unix.sh
+++ other/unix.sh
@@ -1,2 +1,6 @@
 # найти последние измененные файлы
 OFS="$IFS";IFS=$'\n';stat --printf="%y %n\n" $(ls -tr $(find . -type f));IFS="$OFS";
+
+#set sugar logger level
+sed -i -e "s|\berror\b|info|" config_override.php
+sed -i -e "s|\binfo\b|error|g" config_override.php
