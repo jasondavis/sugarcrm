03e18c3171ab9aa557bd7ef1a402679378f2612d
dimaqw <dimaqw@gmail.com>
2015-09-08 16:34:12 +0300

    Update unix.sh

--- other/unix.sh
+++ other/unix.sh
@@ -4,3 +4,6 @@ OFS="$IFS";IFS=$'\n';stat --printf="%y %n\n" $(ls -tr $(find . -type f));IFS="$O
 #set sugar logger level
 sed -i -e "s|\berror\b|info|" config_override.php
 sed -i -e "s|\binfo\b|error|g" config_override.php
+
+# удалить найденные файлы
+find /var/www/hosted/crm75a.smartline.ua/ -name '*ar_SA.lang.php' -delete
